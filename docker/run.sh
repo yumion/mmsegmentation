dab9222ad9168561243ee29b85924cc4fea0b4f0
yumion <a.yamada0703@gmail.com>
2023-11-19 18:05:28 +0900

    feat: update mmengine version

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,8 @@
 CONTAINER_NAME=$1
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+USER_NAME=$(whoami)
+
 
 cd /data2/src/atsushi/mmsegmentation
 docker rm -f $CONTAINER_NAME && \
@@ -10,7 +14,16 @@ docker run \
     -v /data2:/data2 \
     -itd \
     --name $CONTAINER_NAME \
-    mmsegmentation:pytorch1.12.1-cuda11.3-cudnn8-mmcv2.0.1-mmengine0.8.4
+    mmsegmentation:pytorch1.12.1-cuda11.3-cudnn8-mmcv2.1.0-mmengine0.9.1
+
 
 docker exec -it $CONTAINER_NAME bash -c \
-    "cd $PWD && pip install --no-cache-dir -e ."
+    "groupadd -g $GROUP_ID $USER_NAME && useradd -d $PWD -u $USER_ID -g $GROUP_ID $USER_NAME"
+
+docker exec -it $CONTAINER_NAME bash -c \
+    "\
+    cd $PWD \
+    && pip install -r requirements/albu.txt \
+    && pip install -r requirements/optional.txt \
+    && pip install --no-cache-dir -e .\
+    "
